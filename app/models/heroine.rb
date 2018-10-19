5d9322fa81eee878c28da01d9e930d92831c3cb8
srscalia <shelby.scalia@gmail.com>
2018-10-19 11:57:08 -0400

    failed bonus attempt

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,6 +1,7 @@
 class Heroine < ApplicationRecord
   belongs_to :power
   validates :name, presence: true
+  validates :super_name, presence: true
   # not a deliverable, validating name
   validates :super_name, uniqueness: true
   # Deliverable 7: Make sure not two heroines have the same super name.
