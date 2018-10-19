2c750c4d569eb275a2cd218eabaf616faccc92b9
rogerdavidvera <rovera@vassar.edu>
2018-10-19 11:24:00 -0400

    main deliverables done

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,9 @@
 class Heroine < ApplicationRecord
+  # Each heroine can only have one power.
+  belongs_to :power
+
+  validates :name, presence: true
+  validates :super_name, presence: true
+  validates :name, uniqueness: true
+
 end
--- db/migrate/20170725185425_create_heroines.rb
+++ db/migrate/20170725185425_create_heroines.rb
@@ -1,6 +1,7 @@
 class CreateHeroines < ActiveRecord::Migration[5.0]
   def change
     create_table :heroines do |t|
+      t.integer :power_id
       t.string :name
       t.string :super_name
 
