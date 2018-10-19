4acdb1bdd79b7741d69135976204ec023c356649
DDarrow123 <ddarrowphl@gmail.com>
2018-10-19 12:00:51 -0400

    mod 2 code challenge

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,16 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+    #code
+  end
+
+  private
+
+  def heroine_params
+    params.require(:power).permit(:name, :description)
+    #code
+  end
 end
