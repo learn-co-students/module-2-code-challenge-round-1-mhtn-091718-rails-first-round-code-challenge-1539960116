6c42ac9585121b07033f18c9c0ecfd8252dfa5ad
TurtleFeeder <t.choong@yahoo.com>
2018-10-19 11:37:21 -0400

    mod2 assessment without bonus

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,33 @@
 class HeroinesController < ApplicationController
+  before_action :find_heroine, only: [:show]
   def index
     @heroines = Heroine.all
+  end
+
+  def show
+  end
+
+  def new
+    @powers = Power.all
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.valid?
+      redirect_to heroine_path(@heroine)
+    else
+      flash[:error] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+  private
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
+  def find_heroine
+    @heroine = Heroine.find_by(id: params[:id])
   end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,20 @@
 class PowersController < ApplicationController
+    before_action :find_power, only: [:show]
+
   def index
     @powers = Power.all
   end
+
+  def show
+  end
+
+  private
+  def power_params
+    params.require(:power).permit(:name, :description)
+  end
+
+  def find_power
+    @power = Power.find_by(id: params[:id])
+  end
+
 end
