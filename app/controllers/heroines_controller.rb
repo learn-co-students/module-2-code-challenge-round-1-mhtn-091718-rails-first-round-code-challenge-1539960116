37ca982a3e8733bbea024e2e56be07c953638646
Chribtoper <xcstolfax@gmail.com>
2018-10-19 11:55:01 -0400

    finished

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,35 @@
 class HeroinesController < ApplicationController
+  before_action :find_id, only: [:show]
+
   def index
     @heroines = Heroine.all
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(strong_params)
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      flash[:error] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def find_id
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def strong_params
+     params.require(:heroine).permit(:name,:super_name,:power_id)
   end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,17 @@
 class PowersController < ApplicationController
+  before_action :find_id, only: [:show]
+
   def index
     @powers = Power.all
   end
+
+  def show
+  end
+
+  private
+
+  def find_id
+    @power = Power.find(params[:id])
+  end
+
 end
